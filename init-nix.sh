4b9db20c8c8829ccf3756691869c5730710483bd
Thibault Gagnaux <tgagnaux@gmail.com>
2022-11-18 18:46:11 +0100

    fix: manually activate nix-daemon when it is ready

--- init-nix.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-install -d -m 0755 /nix/var /nix/var/log /nix/var/log/nix /nix/var/log/nix/drvs /nix/var/nix{,/db,/gcroots,/profiles,/temproots,/userpool,/daemon-socket} /nix/var/nix/{gcroots,profiles}/per-user
-chgrp nixbld /nix/store
-chmod 1775 /nix/store
-echo "https://nixos.org/channels/nixpkgs-unstable nixpkgs" > /root/.nix-channels
-/nix/store/*nix*/bin/nix-store --load-db < /.reginfo
-/nix/store/*nix*/bin/nix-env -i /nix/store/*nix*
